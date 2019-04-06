70e843f0b33ed605ace9af6f71e99be6ecd6f76c
mahsa <mahmoudmaha@hotmail.com>
2019-04-06 16:11:31 +0200

    add project codereview 13

---
+++ src/AppBundle/Controller/EventsController.php
@@ -0,0 +1,197 @@
+<?php
+namespace AppBundle\Controller;
+// We include the entity that we create in our Controller file
+use AppBundle\Entity\Events;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\integer;
+
+
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+class EventsController extends Controller{
+   /**
+    * @Route("/", name="test1")
+    */
+   public function listAction(Request $request)
+   {   $tests = $this->getDoctrine()->getRepository('AppBundle:Events')->findAll();
+       
+   
+       return $this->render('test1/index.html.twig', array('tests'=>$tests));
+   }
+    /**
+    * @Route("/test1/create", name="test1_create")
+    */
+   public function createAction(Request $request){ 
+    $test1 = new Events;
+
+     $form = $this->createFormBuilder($test1)->add('name', TextType::class, array('attr' => array('class'=> 'form-control', 'style'=>'margin-bottom:15px')))
+     ->add('date', DateTimeType::class, array('attr' => array('class'=> 'form-control', 'style'=>'margin-bottom:15px')))
+       ->add('description', TextareaType::class, array('attr' => array('class'=> 'form-control', 'style'=>'margin-bottom:15px')))
+       ->add('image', textType::class, array('attr' => array('class'=> 'form-control', 'style'=>'margin-bottom:15px'))) 
+       ->add('capacity', textType::class, array('attr' => array('class'=> 'form-control', 'style'=>'margin-bottom:15px')))
+       ->add('email', TextType::class, array('attr' => array('class'=> 'form-control', 'style'=>'margin-bottom:15px')))
+       ->add('phone', TextType::class, array('attr' => array('class'=> 'form-control', 'style'=>'margin-bottom:15px')))
+       ->add('address', TextType::class, array('attr' => array('class'=> 'form-control', 'style'=>'margin-bottom:15px')))
+       ->add('url', TextType::class, array('attr' => array('class'=> 'form-control', 'style'=>'margin-bottom:15px')))
+       ->add('type', TextType::class, array('attr' => array('class'=> 'form-control', 'style'=>'margin-bottom:15px'))) 
+       ->add('save', SubmitType::class, array('label'=> 'Add Event', 'attr' => array('class'=> 'btn-primary', 'style'=>'margin-bottom:15px')))
+       ->getForm();
+       $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+           //fetching data
+           $name = $form['name']->getData();
+           $date = $form['date']->getData();
+           $description = $form['description']->getData();
+           $image = $form['image']->getData();
+           $capacity = $form['capacity']->getData();
+            $email = $form['email']->getData();
+           $phone = $form['phone']->getData();
+           $address = $form['address']->getData();
+           $url = $form['url']->getData();
+           $type = $form['type']->getData();
+
+           $now = new\DateTime('now');
+
+            $test1->setName($name);
+           $test1->setDate($date);
+           $test1->setDescription($description);
+           $test1->setImage($image);
+           $test1->setCapacity($capacity);
+           $test1->setEmail($email);
+           $test1->setPhone($phone);
+           $test1->setAddress($address);
+           $test1->setUrl($url);
+           $test1->setType($type);
+          
+           $em = $this->getDoctrine()->getManager();
+           $em->persist($test1);
+           $em->flush();
+           $this->addFlash(
+                   'notice',
+                   'Events Added'
+                   );
+           return $this->redirectToRoute('test1');
+         }
+       
+
+
+
+
+
+
+   
+
+       return $this->render('test1/create.html.twig', array('form' => $form->createView()));
+     }
+   /**
+    * @Route("/test1/details/{id}", name="test1_details")
+    */
+           public function detailsAction($id){
+               $test1 = $this->getDoctrine()->getRepository('AppBundle:Events')->find($id);
+       return $this->render('test1/details.html.twig', array('test1' => $test1));
+   }
+  
+           /**
+    * @Route("/test1/edit/{id}", name="test1_edit")
+    */
+   public function editAction($id, Request $request){
+
+   $test1 = $this->getDoctrine()->getRepository('AppBundle:Events')->find($id);
+
+$now = new\DateTime('now'); 
+
+          $test1->setName($test1->getName());
+
+           $test1->setDate($test1->getDate());
+           $test1->setDescription($test1->getDescription());
+           $test1->setImage($test1->getImage());
+           $test1->setCapacity($test1->getCapacity());
+           $test1->setEmail($test1->getEmail());
+           $test1->setPhone($test1->getphone());
+           $test1->setAddress($test1->getAddress());
+           $test1->setUrl($test1->getUrl());
+           $test1->setType($test1->getType());
+          
+
+
+
+$form = $this->createFormBuilder($test1)->add('name', TextType::class, array('attr' => array('class'=> 'form-control', 'style'=>'margin-bottom:15px')))
+     ->add('date', DateTimeType::class, array('attr' => array('class'=> 'form-control', 'style'=>'margin-bottom:15px')))
+       ->add('description', TextareaType::class, array('attr' => array('class'=> 'form-control', 'style'=>'margin-bottom:15px')))
+       ->add('image', textType::class, array('attr' => array('class'=> 'form-control', 'style'=>'margin-bottom:15px'))) 
+       ->add('capacity', textType::class, array('attr' => array('class'=> 'form-control', 'style'=>'margin-bottom:15px')))
+       ->add('email', TextType::class, array('attr' => array('class'=> 'form-control', 'style'=>'margin-bottom:15px')))
+       ->add('phone', TextType::class, array('attr' => array('class'=> 'form-control', 'style'=>'margin-bottom:15px')))
+       ->add('address', TextType::class, array('attr' => array('class'=> 'form-control', 'style'=>'margin-bottom:15px')))
+       ->add('url', TextType::class, array('attr' => array('class'=> 'form-control', 'style'=>'margin-bottom:15px')))
+       ->add('type', TextType::class, array('attr' => array('class'=> 'form-control', 'style'=>'margin-bottom:15px'))) 
+       ->add('save', SubmitType::class, array('label'=> 'Edit Event', 'attr' => array('class'=> 'btn-primary', 'style'=>'margin-bottom:15px')))
+       ->getForm();
+       $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+           //fetching data
+           $name = $form['name']->getData();
+           $date = $form['date']->getData();
+           $description = $form['description']->getData();
+           $image = $form['image']->getData();
+           $capacity = $form['capacity']->getData();
+            $email = $form['email']->getData();
+           $phone = $form['phone']->getData();
+           $address = $form['address']->getData();
+           $url = $form['url']->getData();
+           $type = $form['type']->getData();
+
+           $now = new\DateTime('now');
+            $em = $this->getDoctrine()->getManager();
+           $test1 = $em->getRepository('AppBundle:Events')->find($id);
+           $test1->setName($name);
+           $test1->setDate($date);
+           $test1->setDescription($description);
+           $test1->setImage($image);
+           $test1->setCapacity($capacity);
+           $test1->setEmail($email);
+           $test1->setPhone($phone);
+           $test1->setAddress($address);
+           $test1->setUrl($url);
+           $test1->setType($type);
+           
+
+           $em->flush();
+           $this->addFlash(
+                   'notice',
+                   'Events Updated'
+                   );
+           return $this->redirectToRoute('test1');
+       }
+
+
+
+
+       return $this->render('test1/edit.html.twig', array('test1' => $test1, 'form' => $form->createView()));
+   }
+
+   /**
+    * @Route("/test1/delete/{id}", name="test1_delete")
+    */
+   public function deleteAction($id){
+                $em = $this->getDoctrine()->getManager();
+           $test1 = $em->getRepository('AppBundle:Events')->find($id);
+           $em->remove($test1);
+            $em->flush();
+           $this->addFlash(
+                   'notice',
+                   'Events Removed'
+                   );
+            return $this->redirectToRoute('test1');
+   }
+
+}
+
---
+++ src/AppBundle/Entity/Events.php
@@ -0,0 +1,344 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Events
+ *
+ * @ORM\Table(name="events")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\EventsRepository")
+ */
+class Events
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date", type="datetime")
+     */
+    private $date;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="string", length=255)
+     */
+    private $description;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="image", type="blob")
+     */
+    private $image;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="capacity", type="integer")
+     */
+    private $capacity;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="phone", type="string", length=255)
+     */
+    private $phone;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="address", type="string", length=255)
+     */
+    private $address;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="URL", type="string", length=255)
+     */
+    private $uRL;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="Type", type="string", length=255)
+     */
+    private $type;
+
+
+    /**
+     * Get id.
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name.
+     *
+     * @param string $name
+     *
+     * @return Events
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name.
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set date.
+     *
+     * @param \DateTime $date
+     *
+     * @return Events
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date.
+     *
+     * @return \DateTime
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * Set description.
+     *
+     * @param string $description
+     *
+     * @return Events
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description.
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set image.
+     *
+     * @param string $image
+     *
+     * @return Events
+     */
+    public function setImage($image)
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    /**
+     * Get image.
+     *
+     * @return string
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    /**
+     * Set capacity.
+     *
+     * @param int $capacity
+     *
+     * @return Events
+     */
+    public function setCapacity($capacity)
+    {
+        $this->capacity = $capacity;
+
+        return $this;
+    }
+
+    /**
+     * Get capacity.
+     *
+     * @return int
+     */
+    public function getCapacity()
+    {
+        return $this->capacity;
+    }
+
+    /**
+     * Set email.
+     *
+     * @param string $email
+     *
+     * @return Events
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email.
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set phone.
+     *
+     * @param string $phone
+     *
+     * @return Events
+     */
+    public function setPhone($phone)
+    {
+        $this->phone = $phone;
+
+        return $this;
+    }
+
+    /**
+     * Get phone.
+     *
+     * @return string
+     */
+    public function getPhone()
+    {
+        return $this->phone;
+    }
+
+    /**
+     * Set address.
+     *
+     * @param string $address
+     *
+     * @return Events
+     */
+    public function setAddress($address)
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    /**
+     * Get address.
+     *
+     * @return string
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    /**
+     * Set uRL.
+     *
+     * @param string $uRL
+     *
+     * @return Events
+     */
+    public function setURL($uRL)
+    {
+        $this->uRL = $uRL;
+
+        return $this;
+    }
+
+    /**
+     * Get uRL.
+     *
+     * @return string
+     */
+    public function getURL()
+    {
+        return $this->uRL;
+    }
+
+    /**
+     * Set type.
+     *
+     * @param string $type
+     *
+     * @return Events
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * Get type.
+     *
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+}
