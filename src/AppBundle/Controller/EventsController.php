b23ac80226faf181723b890e081b62a0cfd80f6b
mahsa <mahmoudmaha@hotmail.com>
2019-04-07 18:02:25 +0200

    some modification

--- src/AppBundle/Controller/EventsController.php
+++ src/AppBundle/Controller/EventsController.php
@@ -9,7 +9,8 @@ use Symfony\Component\HttpFoundation\Request;
 
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType; 
+use Symfony\Component\Form\Extension\Core\Type\ImageType;
 use Symfony\Component\Form\Extension\Core\Type\integer;
 
 
